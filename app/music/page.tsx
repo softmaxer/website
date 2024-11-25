ff62d5f1770a6a30d0a6fba2f4cfd6326737572b
softmaxer <sriram.vadlamani@proton.me>
2024-11-25 21:48:03 +0100

    Add latest release to work page

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -15,53 +15,34 @@ export default function MusicHome() {
   const isStreamingVisible = useIsVisible(streamingLinksref);
   return (
     <div className="flex flex-col justify-start items-start p-12 md:p-24 lg:p-24 gap-12">
+      <p className="text-2xl">Latest releases </p>
       <div className="flex flex-col md:flex-row lg:flex-row justify-start items-start w-full gap-12 font-solanelRegular">
         <div
           ref={latestReleaseRef}
           className={`flex flex-col w-full ${latestReleaseIsVisible ? "opacity-100" : "opacity-0"} ease-in duration-500`}
         >
-          <p className="text-2xl">Latest release </p>
-          <div className="flex flex-col md:flex-row lg:flex-row gap-6 bg-gray-200 dark:bg-gray-700 rounded-lg justify-center items-center">
-            <iframe
-              style={{ borderRadius: "12px", border: "0px" }}
-              src="https://open.spotify.com/embed/track/6s7Tv5Gqf1hURygPnKBBH8?utm_source=generator&theme=0"
-              width="100%"
-              height="352"
-              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-              loading="lazy"
-            ></iframe>
-            <dl className="text-gray-900 dark:text-white w-full">
-              <div className="flex flex-col pb-3 justify-center items-center">
-                <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">
-                  Written and Procuded by
-                </dt>
-                <dd className="text-lg font-solanelBold">RAM</dd>
-              </div>
-              <div className="flex flex-col py-3 justify-center items-center">
-                <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">
-                  Artwork by
-                </dt>
-                <dd className="text-lg font-solanelBold">
-                  <span className="hover:text-pink-500 transition-all ease-in-out duration-300 font-solanelLight">
-                    <Link
-                      href={"https://sairamved.com"}
-                      className="underline decoration-pink-500 hover:no-underline transition-all ease-in-out duration-300"
-                    >
-                      @sairamved
-                    </Link>
-                  </span>
-                </dd>
-              </div>
-            </dl>
-          </div>
+          <iframe
+            style={{ borderRadius: "12px", border: "0px" }}
+            src="https://open.spotify.com/embed/track/1rYlef5ybNAps3cxGVKW4k?utm_source=generator"
+            width="100%"
+            height="352"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
         </div>
 
         <div
           ref={comingSoonRef}
           className={`flex flex-col w-full ${comingSoonIsVisible ? "opacity-100" : "opacity-0"} ease-in duration-700`}
         >
-          <p className="text-2xl">Coming soon</p>
-          <ComingSoon artistName="RAM" songName="When's tomorrow" />
+          <iframe
+            style={{ borderRadius: "12px", border: "0px" }}
+            src="https://open.spotify.com/embed/track/6s7Tv5Gqf1hURygPnKBBH8?utm_source=generator&theme=0"
+            width="100%"
+            height="352"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
         </div>
       </div>
 
